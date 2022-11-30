8ea561e21d7ee745f2b682094098faec67bb0bf3
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-12-01 00:29:13 +0900

    #1: 런치스크린 예시 구현

---
+++ FreshStorage/FreshStorage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreshStorage
+//
+//  Created by 원동진 on 2022/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+    }
+
+
+}
+
